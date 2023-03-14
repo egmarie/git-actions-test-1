990e4ac8acc0740138e59c7a5e9d4e4cf40963ae
egmarie <egmarie@macbook-pro-409.wireless-1x.unc.edu>
2023-03-14 13:52:04 -0400

    hm2

--- modules/s3.tf
+++ modules/s3.tf
@@ -29,34 +29,18 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
 # tf registry
 
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.static_ar_bucket-1.id
-
+  bucket                  = aws_s3_bucket.static_ar_bucket-1.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-# resource "aws_s3_bucket" "testbucket" {
-#     bucket = "test-terraform-pawan-1"
-#     acl = "private"
-
-#     tags = {
-#         Name  = "test-terraform"
-#         Environment = "test"
-#     }
-# }
-
-# resource "aws_s3_bucket_object" "uploadfile" {
-#   bucket = "test-terraform-pawan-1"
-#   key     = "index.html"
-#   source = "../"
-# }
 resource "aws_s3_bucket_object" "dist" {
   for_each = fileset("../src/", "*")
-  bucket = aws_s3_bucket.static_ar_bucket-1.id
-  key    = each.value
-  source = "../src/${each.value}"
+  bucket   = aws_s3_bucket.static_ar_bucket-1.id
+  key      = each.value
+  source   = "../src/${each.value}"
   # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
-  etag   = filemd5("../src/${each.value}")
+  etag     = filemd5("../src/${each.value}")
 }
